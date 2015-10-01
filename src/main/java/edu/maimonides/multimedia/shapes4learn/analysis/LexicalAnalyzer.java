f7da41e318ff9ab884fd8414973125f3c0e4e003
cmfernandez <fernandez.cesar@maimonides.edu>
2015-10-01 10:41:29 -0300

    Commit
    Test automaticos y lexema analizer funcionando

--- src/main/java/edu/maimonides/multimedia/shapes4learn/analysis/LexicalAnalyzer.java
+++ src/main/java/edu/maimonides/multimedia/shapes4learn/analysis/LexicalAnalyzer.java
@@ -1,8 +1,8 @@
 package edu.maimonides.multimedia.shapes4learn.analysis;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-
 import edu.maimonides.multimedia.shapes4learn.model.Token;
 
 /**
@@ -16,10 +16,193 @@ import edu.maimonides.multimedia.shapes4learn.model.Token;
 public class LexicalAnalyzer {
 
 	public List<Token> analyze(String code) throws LexicalException {
+		
+		String[][] reservada = new String[20][20];
+		//String[] aux;
+		boolean validoNum=false;
+		//boolean validoId=true;
+		boolean ExpArit=true;
+		boolean reservada1=false;
+		boolean id=false;
+		
+		// Completo la matriz token (lexemas - clase)
+		
+		reservada[0][0] = "create";       reservada[0][1] = "crear";
+		
+		reservada[1][0] = "rectangle";    reservada[1][1] = "ShapeRectangulo";
+		
+		reservada[2][0] = "circle"; 	  reservada[2][1] = "ShapeCirculo";
+		
+		reservada[3][0] = ";"; 			  reservada[3][1] = "fin de sentencia";
+		
+		reservada[4][0] = "setbase";      reservada[4][1] = "setear base";
+		
+		reservada[5][0] = "sethight";     reservada[5][1] = "setear altura";
+		
+		reservada[6][0] = "setcolor";     reservada[6][1] = "setear color";
+		
+		reservada[7][0] = "setradius";    reservada[7][1] = "setear radio";
+		
+		reservada[8][0] = "setposition";  reservada[8][1] = "setear posicion";
+		
+		reservada[9][0] = "shape";       reservada[9][1] = "clase shape";
+		
+		reservada[10][0] = ",";           reservada[10][1] = "separador de expresiones";
+		
+		reservada[11][0] = "in";           reservada[11][1] = "'in'";
+		
 		List<Token> tokens = new LinkedList<>();
 		
-		// TODO Implement.
+		// Separo los lexemas
+		String delimitadores= "[\\ \\	\\  \\   \\    \\		]";
+		String lexemas[] = code.split(delimitadores);
 		
+		//Recorro los lexemas y encuentro las Lexemas reservadas del lenguaje
+		for(int i=0; i< lexemas.length; i++) {
+			
+			Token tempToken = new Token();
+			reservada1=false;
+			validoNum=false;
+			//boolean validoId=true;
+			ExpArit=false;
+			reservada1=false;
+			id=false;
+			
+			//System.out.println("\nPosicion: " + i + " <><> Lexema que ingresa: " + lexemas[i]); 
+			
+			//System.out.println("Token: ");
+			
+			for (int j=0;j<=11;j++)
+			{
+				
+				if (lexemas[i].equals(reservada[j][0]))
+				{
+					// Cuando lo encuentro, como se pasa a la linkedlisto token????
+					
+					reservada1=true;
+					
+					tempToken.lexema = reservada[j][0];
+					tempToken.clase = reservada [j][1];
+				}
+			}					
+					// si no es Lexema reservada
+					
+					int x=0;
+					
+					// veo si es numero
+				    for (;x<lexemas[i].length() && Character.isDigit(lexemas[i].charAt(x)) ;x++)
+				     	{ 
+				    		validoNum=true;
+				        }
+				    
+				    if (validoNum) {
+				   
+				    	tempToken.lexema = lexemas[i];
+			        	tempToken.clase = "Numero";
+						
+					} else {
+						if (reservada1==false)
+						{
+						tempToken.lexema = lexemas[i];
+			        	tempToken.clase = "Lexema desconocido";
+						}
+					}
+				    
+				    
+				    //Expresion aritmetica
+
+				    int f=0;
+				    char ant='a';
+				    
+				    for (;(f<lexemas[i].length()) && (reservada1==false);f++)
+			        { 
+				    	
+			        	if (Character.isDigit(lexemas[i].charAt(f)))
+			        			{
+			    					ant=lexemas[i].charAt(f);
+			        			} else
+			        			
+			        			{
+			        				if ((lexemas[i].charAt(f) == '+' || lexemas[i].charAt(f) == '-' ||
+			        					 lexemas[i].charAt(f) == '*' || lexemas[i].charAt(f) == '/')
+			        					&& Character.isDigit(ant))
+			        				{
+			        					ant=lexemas[i].charAt(f);
+			        					ExpArit=true;
+			        				}else
+			        				{
+			        					ExpArit=false;
+			        				}
+			        			}
+			        }
+				    
+			        if (ExpArit)
+			        {
+			        	tempToken.lexema = lexemas[i];
+			        	tempToken.clase = "Expresion Aritmetica";
+
+			        }else
+			        { if (reservada1 == false && validoNum==false)
+			        	{
+			        	tempToken.lexema = lexemas[i];
+			        	tempToken.clase = "Lexema desconocido";
+			        	}
+			        }
+			        
+				    //ID bien
+			        
+			        //System.out.println("Entra en ID el lexema " + lexemas[i]); 
+			        
+			       /* int t=0;
+			        
+				    for (;t<lexemas[t].length() && Character.isLetter(lexemas[t].charAt(t))  && reservada1==false ;t++)
+			        {
+					    tempToken.lexema = lexemas[t];
+					    tempToken.clase = "ID";
+					   // tokens.add(tempToken);	
+			        }*/
+			        
+			        if (lexemas[i].matches("([a-z]|[A-Z]|\\s)+") && reservada1==false) {
+			        	tempToken.lexema = lexemas[i];
+					    tempToken.clase = "ID";
+					    id=true;
+					}  
+				    	        
+				    //color_def
+
+				    //System.out.println("Entra en Color_def el lexema " + lexemas[i]); 
+				    
+
+				    if ( lexemas[i].charAt(0)=='#')
+				    {
+				    	if (lexemas[i].substring(1).matches("([a-f]|[A-F]|[0-9]|\\s)+")) {
+				    		 tempToken.lexema = lexemas[i];
+							 tempToken.clase = "color_def";
+						}else {
+							tempToken.lexema = lexemas[i];
+							tempToken.clase = "Lexema desconocido";
+						}
+				   	}
+				    				    
+				    //Hexadecimal
+				    //System.out.println("Entra en hexadecimal el lexema " + lexemas[i]); 
+				    
+				    if (lexemas[i].matches("([a-f]|[A-F]|[0-9]|\\s)+") && validoNum==false) {
+				    	 tempToken.lexema = lexemas[i];
+						 tempToken.clase = "color_def";
+					}
+
+				    //System.out.println("Token lexema: " + i + " " + tempToken.lexema + " Token clase: " + tempToken.clase);
+				    tokens.add(i,tempToken);				    	
+			}
+		
+		// Se imprimien los token (lexema, clase)
+		
+		/*for (Iterator<Token> iterator = tokens.iterator(); iterator.hasNext();) {
+			Token token = (Token) iterator.next();
+			System.out.println("Lexema: " + token.lexema + "			Clase: " + token.clase);
+		}*/
+		  	
 		return tokens;
 	}
 }
--- src/main/java/edu/maimonides/multimedia/shapes4learn/model/Token.java
+++ src/main/java/edu/maimonides/multimedia/shapes4learn/model/Token.java
@@ -10,6 +10,30 @@ package edu.maimonides.multimedia.shapes4learn.model;
  */
 public class Token {
 
-	// To be implemented.
+	public String lexema;
+	public String clase;
+	
+	public String getLexema() {
+		return lexema;
+	}
+	public void setLexema(String lexema) {
+		this.lexema = lexema;
+	}
+	public String getClase() {
+		return clase;
+	}
+	public void setClase(String clase) {
+		this.clase = clase;
+	}
+	@Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Lexema: ");
+        sb.append(lexema);
+        sb.append(" ||| Clase: ");
+        sb.append(clase);
+        sb.append("\n");
+        return sb.toString();
+    }    
 
 }
