3a69725522f026522ad0139702808809e1a8eea8
cmfernandez <fernandez.cesar@maimonides.edu>
2015-10-28 07:43:50 -0300

    SyntacticAnalyzer
    Se agrega la clase SyntacticAnalizer

--- src/main/java/edu/maimonides/multimedia/shapes4learn/analysis/SyntacticAnalyzer.java
+++ src/main/java/edu/maimonides/multimedia/shapes4learn/analysis/SyntacticAnalyzer.java
@@ -1,7 +1,8 @@
 package edu.maimonides.multimedia.shapes4learn.analysis;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
-
 import edu.maimonides.multimedia.shapes4learn.model.AST;
 import edu.maimonides.multimedia.shapes4learn.model.Token;
 
@@ -15,14 +16,405 @@ import edu.maimonides.multimedia.shapes4learn.model.Token;
  */
 public class SyntacticAnalyzer {
 
+	private String sentencia;
+	private String lookahead;
+	private Token token;
+	private Iterator<Token> iterator;
+
 	public SyntacticAnalyzer() {
 	}
 
 	public AST analyze(List<Token> tokens) throws SyntacticException {
+		
+		List<String> sentencias = new LinkedList<>();
 		AST ast = new AST();
 
-		// TODO Implement.
-
+		sentencia = new String();
+		
+		
+		for (Iterator<Token> iterator = tokens.iterator(); iterator.hasNext();) {
+			Token token = (Token) iterator.next();
+			
+			if ((token.lexema.charAt(0) == ';')){
+				
+				sentencias.add(sentencia + ';');
+				
+				sentencia = new String();
+				
+				
+			}else{
+				sentencia = sentencia.concat(' ' +token.lexema);	
+				
+				
+				
+				}
+			
+		}
+		
+		for (Iterator<String> iterator = sentencias.iterator(); iterator.hasNext();) {
+			String sent = iterator.next();
+			System.out.println("Setencia: " + sent);
+			
+		}
+	
+		checkSent(tokens);  
+		
+		
+		  	
 		return ast;
+	}
+	
+public void checkSent(List<Token> tokens){
+		
+		for (iterator = tokens.iterator(); iterator.hasNext();) {
+			
+			token = (Token) iterator.next();
+			
+			lookahead = token.getClase();
+			
+			//Cambiar clase
+		
+			
+		
+			if (lookahead == "crear"){
+				checkCreate(lookahead);
+			}
+		
+
+			if (lookahead == "setear color"){
+				checkSetColor(lookahead);
+			}
+			
+			if (lookahead == "setear base"){
+				checkSetBase(lookahead);
+			}
+		
+			if (lookahead == "setear altura"){
+				checkSetHeight(lookahead);
+			}
+			
+			if(lookahead == "setear radio"){
+				checkRadio(lookahead);
+			}
+		
+			if(lookahead == "setear posicion"){
+				checkPosition(lookahead);
+			}
+		}
+		
+	}
+
+
+//  setposition [expression],[expression] in shape [id] ;
+
+private void checkPosition(String string) {
+	System.out.println("setposition [expression],[expression] in shape [id] ;");
+	matchSetPosition(string);
+	checkExpresion(lookahead);
+	matchComa(lookahead);
+	checkExpresion(lookahead);
+	matchIn(lookahead);
+	matchShape(lookahead);
+	matchId(lookahead);
+	matchFin(lookahead);
+	
+	
+}
+
+
+//	setradius [expression] in circle [id] ;
+
+private boolean matchComa(String string) {
+	if(string == "separador de expresiones"){
+		System.out.println("Es Coma");
+		token = (Token) iterator.next();
+		lookahead = token.getClase();
+		return true;
+	}
+	System.out.println("No es Coma. Error");
+	System.out.close();
+	return false;
+	
+}
+
+private boolean matchSetPosition(String string) {
+	if(string == "setear posicion"){
+		System.out.println("Es setPosition");
+		token = (Token) iterator.next();
+		lookahead = token.getClase();
+		return true;
+	}
+	System.out.printf("Vino %s, se esperaba setPosition.", string);
+	System.out.close();
+	return false;
+	
+}
+
+private void checkRadio(String string) {
+	System.out.println("setradius [expression] in circle [id] ;");
+	matchSetRadio(string);
+	checkExpresion(lookahead);
+	matchIn(lookahead);
+	matchCircle(lookahead);
+	matchId(lookahead);
+	matchFin(lookahead);
+	
+}
+
+private boolean matchSetRadio(String string) {
+	if(string == "setear radio"){
+		System.out.println("Es setRadio");
+		token = (Token) iterator.next();
+		lookahead = token.getClase();
+		return true;
+	}
+	System.out.printf("Vino %s, se esperaba setRadio.", string);
+	System.out.close();
+	return false;
+		
+	}
+
+	//	setcolor [color_def] in shape [id];
+
+	private void checkSetColor(String string) {
+		
+		System.out.println("setcolor [color_def] in shape [id] ;");
+		matchSetColor(string);		
+		matchColorDef(lookahead);
+		matchIn(lookahead);
+		matchShape(lookahead);
+		matchId(lookahead);
+		matchFin(lookahead);
+		
+	}
+	
+	private boolean matchShape(String string) {
+		if(string == "clase shape"){
+			System.out.println("Es shape");
+			token = (Token) iterator.next();
+			lookahead = token.getClase();
+			return true;
+		}
+		System.out.printf("Vino %s, se esperaba 'shape'", string);
+		System.out.close();
+		return false;
+		
+		
+	}
+
+	private boolean matchIn(String string) {
+		if(string == "'in'"){
+			System.out.println("Es in");
+			token = (Token) iterator.next();
+			lookahead = token.getClase();
+			return true;
+		}
+		System.out.printf("Vino %s, se esperaba 'in'", string);
+		System.out.close();
+		return false;
+		
+		
+	}
+
+	
+//	setheight [expression] in rectangle [id] ;
+	
+	private void checkSetHeight(String string) {
+		System.out.println("setheight [expression] in rectangle [id] ;");
+		matchSetHeight(string);		
+		checkExpresion(lookahead);
+		matchIn(lookahead);
+		matchRectangle(lookahead);
+		matchId(lookahead);
+		matchFin(lookahead);
+		
+	}
+	
+
+	
+	private boolean matchSetHeight(String string) {
+		if(string == "setear altura"){
+			System.out.println("Es setHeight");
+			token = (Token) iterator.next();
+			lookahead = token.getClase();
+			return true;
+		}
+		System.out.printf("Vino %s, se esperaba setHeight.", string);
+		System.out.close();
+		return false;
+		
+	}
+
+//  setbase [expression] in rectangle [id] ;
+	
+	private void checkSetBase(String string) {
+		
+		System.out.println("setbase [expression] in rectangle [id] ;");
+		matchSetBase(string);		
+		checkExpresion(lookahead);
+		matchIn(lookahead);
+		matchRectangle(lookahead);
+		matchId(lookahead);
+		matchFin(lookahead);
+		
+	}
+
+	private boolean matchSetBase(String string) {
+		if(string == "setear base"){
+			System.out.println("Es setBase");
+			token = (Token) iterator.next();
+			lookahead = token.getClase();
+			return true;
+		}
+		System.out.printf("Vino %s, se esperaba setBase.", string);
+		System.out.close();
+		return false;
+		
+	}
+
+	private void checkExpresion(String string) {
+		
+		if (string == "Numero"){
+			System.out.println("Es numero");
+			token = (Token) iterator.next();
+			lookahead = token.getClase();
+			checkExpresion(lookahead);
+		} 
+		
+	
+			
+		
+	}
+
+	private void matchFin(String string) {
+		
+		if (string == "fin de sentencia"){
+			
+			if (iterator.hasNext()){
+			
+			System.out.println("[;] Fin de sentencia");
+			}
+			else {
+				System.out.println("[;] Fin de sentencia");
+				System.out.println("Terminaron las sentencias a analizar");
+				
+			}
+		}
+		
+	}
+
+	private boolean matchSetColor(String string) {
+		if(string == "setear color"){
+			System.out.println("Es setColor");
+			token = (Token) iterator.next();
+			lookahead = token.getClase();
+			return true;
+		}
+		System.out.printf("Vino %s, se esperaba setcolor \n", string);
+		System.out.close();
+		return false;
+		
+	}
+
+	private boolean matchColorDef(String string) {
+		if(string == "color_def"){
+		System.out.println("Es un color");
+		token = (Token) iterator.next();
+		lookahead = token.getClase();
+		return true;
+	}
+		System.out.printf("Vino %s, se esperaba un color del tipo #AAA111 \n", string);
+		System.out.close();
+		return false;
+	
+		
+	}
+
+//	create rectangle|circle [id];
+	
+	private void checkCreate(String string) {
+		matchCreate(string);		
+		checkShape(lookahead);
+		matchId(lookahead);
+		matchFin(lookahead);
+		
+		}
+		
+	
+
+	private boolean matchId(String string) {
+		
+		if(string == "ID"){
+			System.out.println("Es ID.");
+			token = (Token) iterator.next();
+			lookahead = token.getClase();
+			return true;
+		} else {
+			System.out.printf("Vino %s, se esperaba un ID valido \n ", string);
+			System.out.close();
+			return false;
+		}
+		
+	}
+
+	private void checkShape(String string) {
+		
+		if (string == "ShapeCirculo"){
+			System.out.println("Es circle");
+			matchCircle(lookahead);
+			
+		}else {
+			if (string == "ShapeRectangulo"){
+				System.out.println("Es rectangle");
+				matchRectangle(lookahead);
+			
+			} else {
+				System.out.printf("Vino %s, se esperaba una figura \n ", string);
+				System.out.close();
+				
+			}
+		
+		
+		}
+
+	}
+
+	private boolean matchRectangle(String string) {
+		if(string == "ShapeRectangulo"){
+			token = (Token) iterator.next();
+			lookahead = token.getClase();
+			return true;
+		} else {
+		System.out.printf("Vino %s, se esperaba un rectangulo \n ", string);
+			System.out.close();
+			return false;
+		}
+		
+	}
+
+	private boolean matchCircle(String string) {
+		if(string == "ShapeCirculo"){
+			token = (Token) iterator.next();
+			lookahead = token.getClase();
+			return true;
+		}
+		
+		System.out.close();
+		return false;
+		
+		
+	}
+
+	private boolean matchCreate(String string) {
+		if(string == "crear"){
+			System.out.println("Es create");
+			token = (Token) iterator.next();
+			lookahead = token.getClase();
+			return true;
+		}
+		System.out.printf("Vino %s, se esperaba 'create' \n ", string);
+		System.out.close();
+		return false;
+		
 	}
 }
